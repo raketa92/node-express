9185e54092da0c9df34fb7c9691604fc764c1000
raketa92 <nazar.erdem@gmail.com>
2020-03-31 18:02:23 +0500

    ch3 le11

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const Course = require('../models/course')
 const router = Router()
 
 
@@ -7,6 +8,14 @@ router.get('/', (req, res) => {
         title: 'Добавить курс',
         isAdd: true
     })
+})
+
+router.post('/', async (req, res) => {
+    const course = new Course(req.body.title, req.body.price, req.body.img)
+    await course.save()
+
+
+    res.redirect('/courses')
 })
 
 module.exports = router
